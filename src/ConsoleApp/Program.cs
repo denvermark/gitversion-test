ce40d4ee03bd4f0c9f3f8b91c6d72377eabb1512
Mark Jeffries <mjeffries@ssbinfo.com>
2019-03-01 08:31:06 -0700

    +semver: Minor adding a new feature

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test change 3");
+            Console.WriteLine("Test change 3 ");
         }
     }
 }
